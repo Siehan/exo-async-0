ad81229ad43ebf795f4d732a34960664a03f82ac
Siehan <memain.ye.sylvie@gmail.com>
2021-04-09 15:34:26 +0200

    done

---
+++ correctMe.js
@@ -0,0 +1,24 @@
+/*
+correctMe.js
+
+Ce programme asynchrone ne fonctionne pas.
+Ajouter le code nécessaire pour faire fonctionner ce programme.
+const fsPromises = require('fs/promises')
+
+*/
+const chalk = require("chalk");
+const fsPromises = require("fs/promises");
+
+console.log(chalk.red("--------------------------"));
+
+const myReadFile = async () => {
+  try {
+    // readFile retourne une promise et pas le texte du fichier!!!
+    const txt1 = await fsPromises.readFile("hello.txt", "utf-8"); // exécution de readFile
+    console.log(txt1);
+  } catch (e) {
+    // On gère les erreurs comme dans un code synchrone
+    console.log(e.message);
+  }
+};
+myReadFile();
